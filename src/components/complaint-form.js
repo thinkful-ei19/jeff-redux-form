84977e29cf1382aea9975f8d5a162ab2c868c1d3
Jeff Vega <jeff@intellibliss.com>
2018-04-18 13:54:49 -0700

    working and sends to API

--- src/components/complaint-form.js
+++ src/components/complaint-form.js
@@ -1,12 +1,55 @@
 import React from 'react';
-import {reduxForm, Field} from 'redux-form';
+import {reduxForm, Field, SubmissionError,focus} from 'redux-form';
 import Input from './input'
 import { required, nonEmpty, exactLength, onlyNumbers } from '../validators';
 const exactLength5 = exactLength(5);
 
 export class ContactForm extends React.Component {
     onSubmit(values) {
-        console.log(values);
+        console.log(values,'OnSubmit')
+        return fetch('https://us-central1-delivery-form-api.cloudfunctions.net/api/report', {
+            method: 'POST',
+            body: JSON.stringify(values),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => {
+                if (!res.ok) {
+                    if (
+                        res.headers.has('content-type') &&
+                        res.headers
+                            .get('content-type')
+                            .startsWith('application/json')
+                    ) {
+                        // It's a nice JSON error returned by us, so decode it
+                        return res.json().then(err => Promise.reject(err));
+                    }
+                    // It's a less informative error returned by express
+                    return Promise.reject({
+                        code: res.status,
+                        message: res.statusText
+                    });
+                }
+                return;
+            })
+            .then(() => console.log('Submitted with values', values))
+            .catch(err => {
+                const {reason, message, location} = err;
+                if (reason === 'ValidationError') {
+                    // Convert ValidationErrors into SubmissionErrors for Redux Form
+                    return Promise.reject(
+                        new SubmissionError({
+                            [location]: message
+                        })
+                    );
+                }
+                return Promise.reject(
+                    new SubmissionError({
+                        _error: 'Error submitting message'
+                    })
+                );
+            });
     }
     render() {
         let successMessage;
@@ -29,37 +72,33 @@ export class ContactForm extends React.Component {
             <div className="compl8-wrapper">
             <header>
               <h1>Report a problem with your delivery</h1>
+                        {successMessage}
+                    {errorMessage}
             </header>
             <main>
             <form
                 onSubmit={this.props.handleSubmit(values =>
                     this.onSubmit(values)
                 )}>
-                   {successMessage}
-                    {errorMessage}
-                <label htmlFor="name">Tracking Number</label><br/>
-                <Field name="name" id="name" type="text" component={Input}
+                   
+                <Field name="trackingNumber" id="trackingNumber" label="Tracking Number" component={Input}
                 validate={[
                     required,
                     nonEmpty,
                     exactLength5,
                     onlyNumbers]} />
-                <br/><br/>
-                <label htmlFor="select-option">What is our issue?</label>
+                <br/>
                 {/* need this to be  seletction */}
-                <Field component={Input} element="select" name="someDropdown">
-                <option value="Option 1">My delivery hasn't arrived </option>
-                <option value="Option 2">The wrong item was deliverd</option>
-                <option value="Option 3">Part of my order arrived damaged</option>
-                <option value="Option 4">Other(give more details below)</option>
+                <Field component={Input} element="select"  name="issue" id="issue" label="What is our issue?">
+                <option value="not-delivered">My delivery hasn't arrived </option>
+                <option value="wrong-item">The wrong item was deliverd</option>
+                <option value="damaged">Part of my order arrived damaged</option>
+                <option value="other">Other(give more details below)</option>
                 </Field>
                 <br/>
-                <label htmlFor="message">Message</label><br/>
                 
-                <Field name="message" id="message" component={"textarea"} /><br/>
-                <button type="submit" disable={
-                    this.props.pristine||
-                    this.props.sumbitting}>Send Complaint</button>
+                <Field name="details" id="details" component={Input} element="textarea" label="Give more deatils(Optional)"/><br/>
+                <button type="submit">Send Complaint</button>
             </form>
             </main>
       </div>
@@ -68,5 +107,7 @@ export class ContactForm extends React.Component {
 }
 
 export default reduxForm({
-    form: 'complaint'
+    form: 'compliant',
+    onSubmitFail: (errors, dispatch) =>
+        dispatch(focus('contact', Object.keys(errors)[0]))
 })(ContactForm);
